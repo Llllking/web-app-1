8ede40a7bc387d4c8ce3622fc2825821ab40ebd7
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-06-13 05:47:34 +0100

    logging using a logger instead of sout which is recommended

--- src/main/java/com/ismail/springboot/my_web_app/login/LoginController.java
+++ src/main/java/com/ismail/springboot/my_web_app/login/LoginController.java
@@ -1,14 +1,20 @@
 package com.ismail.springboot.my_web_app.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap model){
+        logger.info("request param is {}", name);
         model.put("name", name);
         return "login";
     }
